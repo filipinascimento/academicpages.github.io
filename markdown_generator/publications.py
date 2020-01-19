e6d90940ab72a5dab9a78c7fdab7fe15f4ec89e0
Filipi Nascimento Silva <filipi@Nemesis.local>
2020-01-19 13:31:37 -0500

    Updated template

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -23,22 +23,26 @@
 
 # In[2]:
 
+import pandas as pd
 
 
+# ## Import TSV
+# 
+# Pandas makes this easy with the read_csv function. We are using a TSV, so we specify the separator as a tab, or `\t`.
+# 
+# I found it important to put this data in a tab-separated values format, because there are a lot of commas in this kind of data and comma-separated values can get messed up. However, you can modify the import statement, as pandas also has read_excel(), read_json(), and others.
 
-import bibtexparser
-from bibtexparser.bparser import BibTexParser
-from bibtexparser.bwriter import BibTexWriter
-from bibtexparser.bibdatabase import BibDatabase
-from bibtexparser.customization import convert_to_unicode
+# In[3]:
 
-with open('citations.bib') as bibtex_file:
-    parser = BibTexParser()
-    parser.customization = convert_to_unicode
-    bib_database = bibtexparser.load(bibtex_file, parser = parser)
-    print(bib_database.entries)
+publications = pd.read_csv("publications.tsv", sep="\t", header=0)
+publications
 
-publications = bib_database.entries;
+
+# ## Escape special characters
+# 
+# YAML is very picky about how it takes a valid string, so we are replacing single and double quotes (and ampersands) with their HTML encoded equivilents. This makes them look not so readable in raw format, but they are parsed and rendered nicely.
+
+# In[4]:
 
 html_escape_table = {
     "&": "&amp;",
@@ -58,79 +62,44 @@ def html_escape(text):
 # In[5]:
 
 import os
-for item in publications:
+for row, item in publications.iterrows():
     
-    md_filename = str(item["year"]) + "-" + item["ID"].replace(":","-") + ".md"
-    html_filename = item["ID"].replace(":","-")
-    year = item["year"]
-
-    db = BibDatabase();
-    writer = BibTexWriter()
-    db.entries = [item];
-
-    with open("../files/bibtex/" + html_filename + ".bib", 'w') as bibfile:
-        bibfile.write(writer.write(db));
-
-
-    md = "---\ntitle: \""   + item["title"] + '"\n'
+    md_filename = str(item.pub_date) + "-" + item.url_slug + ".md"
+    html_filename = str(item.pub_date) + "-" + item.url_slug
+    year = item.pub_date[:4]
+    
+    ## YAML variables
+    
+    md = "---\ntitle: \""   + item.title + '"\n'
     
     md += """collection: publications"""
     
-    md += """\npermalink: /publication/""" + (str(item["year"]) + "-" + item["ID"].replace(":","-"))
-    #md += """\nexcerpt: 'test'"""
+    md += """\npermalink: /publication/""" + html_filename
     
-    if("author" in item):
-        authstr = item["author"];
-        authors = "";
-        numcommas = 0;
-        if ("," in item["author"]):
-          numcommas += 1
-          authstrauthors = authstr.split(" and ")
-          for ia, author in enumerate(authstrauthors):
-            if ("," in author):
-              authorparts = author.split(", ")
-              # the first part [0] is last name, needs to become last
-              # get and remove the first part, then append it as last
-              lastname = authorparts.pop(0)
-              authorparts.append(lastname)
-              authorfirstlast = " ".join(authorparts)
-              authstrauthors[ia] = authorfirstlast
-          authors = ", ".join(authstrauthors);
-        else:
-          authors = item["author"];
-        authors = authors.replace('{\lowercase{d}a F}','da F');
-        md += "\nauthors: \""  + html_escape(authors)+'"';
-
-    md += "\ndate: " + str(item["year"])+"-01-01";
+    if len(str(item.excerpt)) > 5:
+        md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
     
-    venues = [];
-
-    if("journal" in item):
-        venues.append(item["journal"]);
-        if("volume" in item):
-            venues.append("v. "+item["volume"]);
-        if("number" in item):
-            venues.append("n. "+item["number"]);
-        if("pages" in item):
-            venues.append("p. "+item["pages"]);
-        venue = ", ".join(venues);
-        md += "\nvenue: '" + html_escape(venue) + "'";
-
-    md += "\nbibtex: \""   + html_filename + ".bib\"";
+    md += "\ndate: " + str(item.pub_date) 
     
-    if "link" in item:
-        md += "\npaperurl: '" + item["link"] + "'";
+    md += "\nvenue: '" + html_escape(item.venue) + "'"
     
-    if "doi" in item:
-        md += "\ndoi: " + item["doi"];
-    #md += "\ncitation: '" + "BibTex" + "'";
+    if len(str(item.paper_url)) > 5:
+        md += "\npaperurl: '" + item.paper_url + "'"
     
-    md += "\n---";
+    md += "\ncitation: '" + html_escape(item.citation) + "'"
+    
+    md += "\n---"
     
     ## Markdown description for individual page
     
-    #md += "\n[BibTex](http://filipinascimento.github.io/files/bibtex/" + html_filename + ".bib" + ")\n" 
+    if len(str(item.paper_url)) > 5:
+        md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
         
+    if len(str(item.excerpt)) > 5:
+        md += "\n" + html_escape(item.excerpt) + "\n"
+        
+    md += "\nRecommended citation: " + item.citation
+    
     md_filename = os.path.basename(md_filename)
        
     with open("../_publications/" + md_filename, 'w') as f:
